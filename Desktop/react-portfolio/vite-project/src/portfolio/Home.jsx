1d004cf9aba33aefb5240089246d58a3a2377c57
rramesh56816-rgb <rramesh56816@gmail.com>
2026-08-19 08:39:54 +0530

    Update mobile responsive CSS

--- Desktop/react-portfolio/vite-project/src/portfolio/Home.jsx
+++ Desktop/react-portfolio/vite-project/src/portfolio/Home.jsx
@@ -1,7 +1,8 @@
-//import { StrictMode } from 'react';
+
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from "react";
 import { FaLinkedin, FaGithub, FaWhatsapp, FaInstagram } from "react-icons/fa";
+import emailjs from '@emailjs/browser';
 import './portfolio.css';
 
 function Home() {
@@ -12,7 +13,7 @@ function Home() {
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 2000); 
   }, []);
 
   if (loading) {
@@ -22,10 +23,14 @@ function Home() {
       </div>
     );
   }
+  const sendEmail = (e) => {
+  e.preventDefault();
+
+  emailjs.sendForm("service_vf2t7jm","template_z48ve87",e.target,"g_o-BtWPN2jg2lGVX")
+};
 return(
     <section>
 
-        <h4 className='name'>Rambo</h4>
         <div className='ramboimg'>
      <div className='intro'>
         <h2>Hello,it's Ramesh </h2>
@@ -34,69 +39,61 @@ return(
             Turning complex ideas into simple user experiences.<br />
             Building the future, one project at a time.</p>
             <div className='logo'  data-aos="fade-up">
-               <a className='l' href="https://linkedin.com" target="_blank">
+               <a className='l' href="https://www.linkedin.com/in/ramesh-ramesh-a807042a8?utm_source=share_via&utm_content=profile&utm_medium=member_android" target="_blank">
                <FaLinkedin />
                </a>
-               <a className='l' href="https://github.com" target="_blank">
+               <a className='l' href="https://github.com/rramesh56816-rgb" target="_blank">
                <FaGithub />
                </a>
-               <a className='l' href="https://web.whatsapp.com" target="_blank">
+               <a className='l' href="https://wa.me/917695813972?text=Hello%20Ramesh" target="_blank">
                <FaWhatsapp />
-               </a>
-               <a className='l' href="https://www.instagram.com/" target="_blank">
-               <FaInstagram/>
                </a>
             </div>
 
             <div className='btn-grp' data-aos="fade-down">
-            <button  className='A-btn'  onClick={() => { const link = document.createElement("a");  link.href = "/my resume.pdf"; link.download = "Resume.pdf";  link.click();}}>download resume</button>
+            <button  className='A-btn'  onClick={() => { const link = document.createElement("a");  link.href = `${import.meta.env.BASE_URL}my resume.pdf`; link.download = "Resume.pdf";  link.click();}}>download resume</button>
             <button  className='B-btn' onClick={() => { document.getElementById('contact').scrollIntoView({ behavior:"smooth" });}}>Contact Me</button>
             </div> 
-             <hr className='f-line' />
+          
      </div>
-     <img className='rambo2' src='./rambo2.png' />  
+     <img className='rambo2' src='./ramesh.png' />  
        
           </div>
-         
+
+       <div className='about-me'> 
      <h1 className='a-m'>About Me</h1>
-
-     <div className='rambo3img'>
-      <section className='edit'>
-        <img className='rambo' src='./ramesh2.png' />
-      </section>
      <div className='about2'>
-     <h1 className='a-t'>WHO  <span>I</span>  AM</h1>
-
      <div className='about'>
           <h2 className='a-h'><span>Frontend Developer</span> & UI Enthusiast</h2>
-        <p className='a-p'> I am a passionate Frontend Developer skilled in building responsive and <br />
-            user-friendly websites using React, JavaScript, and CSS. I love creating <br />
-            clean UI designs and turning ideas into real projects. I enjoy developing <br />
-            modern web applications with smooth user experiences and well-structured code.<br />
-            My goal is to create websites that are visually appealing, fast, and easy <br />
-            for users to interact with. I am always interested in learning new <br />
+        <p className='a-p'> I am a  Frontend Developer skilled in building responsive and 
+            user-friendly websites using React, JavaScript, and CSS. I love creating 
+            clean UI designs and turning ideas into real projects. I enjoy developing 
+            modern web applications with smooth user experiences and well-structured code.
+            My goal is to create websites that are visually appealing, fast, and easy 
+            for users to interact with. I am always interested in learning new 
             technologies, improving my skills, and building creative solutions through code.
         </p>
      </div>
      </div>
          </div>
+
+         <section className='j-ebox'>
         <div className='journey-box' data-aos="fade-right" >
         <h2 className='journey'>My Journey</h2>
-        <p>I started my journey in web development<br /> with a passion for creating<br />
-           beautiful and interactive websites.<br /> I enjoy learning new technologies<br />
-           and improving my skills through <br />real-world projects.</p>
+        <p>I started with a passion for creating<br />
+           beautiful and interactive websites.<br /> I learning new technologies<br />
+           and improving my skills through .</p>
         </div>
         <div className='expertise-box'  data-aos="fade-left">
             <h2 className='expertise'>My Expertise</h2>
             <p> Frontend Development <br />
               → Building responsive websites using React & JavaScript<br />
-                UI Design<br />
-              → Creating clean and user-friendly interfaces<br />
                 Problem Solving <br />
               → Turning ideas into practical solutions</p>
         </div>
+
         <div className='journey-box' data-aos="fade-down">
-            <h2 className='journey'>What I Love Building</h2>
+            <h2 className='journey'>What I Build</h2>
             <p> Responsive Websites<br />
                 Interactive User Interfaces<br />
                 Modern Web Applications<br />
@@ -108,8 +105,7 @@ return(
                 build impactful web applications,<br />
                 and continuously learn new technologies.</p>
         </div>
-
-        <hr className='f-line' />
+        </section>
         
         <h1 className='myskill'>My  Skill</h1>
 
@@ -153,10 +149,10 @@ return(
             </div>
         </div>
 
-        <hr className='f-line' />
+      
 
         <div className='project'>
-           <h1 className='myproject'>My <span>Project</span></h1>
+           <h1 className='myproject'>My <span>Project</span>span</h1>
           <div className='project-box'>
            <section className='portalimg'  data-aos="fade-right">
              <img className='projectimg' src='./portal-h.png' />
@@ -185,32 +181,10 @@ return(
               <h3 className='key'>.REACT</h3>
               <h3 className='key'>.REST API</h3>
             </div>
-
+            <button className="c-btn"onClick={() =>window.open(  "https://rramesh56816-rgb.github.io/Employee--management-portal/","_blank")}> Click Here</button>
            </section>
-           
           </div>
-          <div className='project-box'>
-           <section className='portalimg' data-aos="fade-left">
-             <img className='portfolioimg' src='./portfolio.png' />
-             <img className='portfolio2img' src='./portfolio2.png' />
-           </section>
-
-           <section className='p-des'>
-           <h2 className='portalh1'>Title:</h2>
-           <h1 className='portaltitle'>Personal Portfolio Website</h1>
-            <h2 className='portalh1'>Description:</h2>
-            <p className='project-p'>A personal portfolio website built using HTML, CSS,<br /> and <br /> JavaScript to showcase my skills,<br /> projects, and frontend development journey..</p>
-          
-            <h2 className='portalh1'>Tec Stack:</h2>
-            <div className='feature'>
-              <h3 className='key'> .HTML</h3>
-              <h3 className='key'> .CSS</h3>
-              <h3 className='key'> .JAVASCRIPT</h3>
-            </div>
-
-           </section>
            
-          </div>
           <div className='project-box'>
            <section className='portalimg' data-aos="fade-right">
              <img className='smartimg' src='./smarttask.png' />
@@ -244,7 +218,7 @@ return(
 
         </div>
 
-        <hr className='f-line' />
+        
 
         <div className='Education'>
           <h1 className='heading-education'>Edu<span>c</span>ation</h1>
@@ -259,25 +233,37 @@ return(
          <p>Merit Polyitecnic Colloge</p>
          <p>Year: 2023 - 2026</p>
         </div>
-        .</section>
+        </section>
         </div>
 
-         <hr className='f-line' />
-
+         
          <div id='contact'>
          <h1 className='heading-education'>Contact <span>Me</span></h1>
-         <div className='contact'>
+
+         <div className='contact-box'>
+
+       <div className='contact'>
         <p>I'm interested in web development opportunities. Feel free to connect with me.</p>
         <p>Email: rramesh56816@gmail.com</p>
         <p>Phone: +91 7695813972</p>
        </div>
+       
+       <section className='user-box'>
+        <form onSubmit={sendEmail}>
+        <input className='usermesg' name="user_name"placeholder="Your Name"/>
+        <input className='usermesg' name="user_email"placeholder="Your Email"/>
+        <textarea className='usermesg' name="message"placeholder="Message"/>
+        <button className='A-btn' type="submit">Send</button>
+        </form>
+       </section>
 
+       </div>
+       
        <footer className='footer'>
             <p>© 2026 Rambo. Frontend Developer.</p>
-            <p>THANK YOU.!!!</p>
+            <p>Built by Rambo | React • CSS • JavaScript</p>
        </footer>
 
-        <hr className='f-line' />
       </div>
         </div>
     </section>
